cf2d39a51423d25ed7ceef8ac50a4c5216faa1c0
Sreeroop <adukkadu@hm.edu>
2024-02-28 01:27:54 +0100

    Update education and skills information

--- commands.js
+++ commands.js
@@ -8,7 +8,7 @@ whois = [
   "I'm a computer science student, a mechatronics engineer, and a lifelong learner.",
   "I am living in Munich, Germany at the moment, studying as a Dual Student at Munich University of Applied Sciences.",
   "Before this, I lived in Singapore for four and a half years. I got my diploma in Mechatronics Engineering there.",
-  "and I was deemed worthy of the Poly goes UAS Scholarship to study in Germany for my Bachelor's degree sponsored by Rohde & Schwarz and EDB Singapore.",
+  "and I was deemed worthy of the Poly goes UAS Scholarship to study in Germany for my Bachelor's degree sponsored by Rohde & Schwarz and Economic Development Board (EDB) Singapore.",
   "Should you wish to unravel the mysteries of my education and the marvels of my skills, simply cast the spell <span class=\"command\">education</span> or <span class=\"command\">skills</span>.",
   "If you want to know more about my professional life, you can check out my LinkedIn profile. Here's the link: <a href=\"" + linkedin + "\" target=\"_blank\">" + linkedin + "</a>.",
   "<br>"
@@ -28,10 +28,13 @@ projects = [
 
 education = [
   "<br>",
-  "Currently, Sreeroop's deep into his studies at Munich University of Applied Sciences, working towards his Bachelor's in Computer Science. It's a journey that began in 2022 and will keep him busy until around 2026.",
-  "Sreeroop graduated from Temasek Polytechnic with a Diploma in Mechatronics Engineering, Singapore. Those were the years between 2018 and 2021.",
-  "During the final year, he specialized in Semiconductor technology modules that focused on the IC fabrication process, cleanroom equipment and technology, and the physics behind them.",
-  "After graduation from Temasek Polytechnic, Sreeroop was indulged in a 10 Months Long Intensive German Language Course until B2 level at Goethe Institute, Singapore and worked at Rohde & Schwarz at the same time.",
+  "Sreeroop is currently immersed in his studies at Munich University of Applied Sciences, pursuing a Bachelor's degree in Computer Science. It's a journey that began in 2022 and will keep him busy until around 2026.",
+  "<br>",
+  "Sreeroop graduated from Temasek Polytechnic with a 'Diploma with Merit in Mechatronics Engineering', Singapore, where he also earned the distinction of being a Course Medallist ! Those were the years between 2018 and 2021.",
+  "In his final year, he specialized in Semiconductor technology modules that focused on the IC fabrication process, cleanroom equipment and technology, and the underlying physics.",
+  "His final year project 'Gesture Recognition Kit' won the merit award from School of Engineering, Temasek Polytechnic",
+  "<br>",
+  "After graduation from Temasek Polytechnic, Sreeroop was indulged in a 10 Months Long Intensive German Language Course until B2 level at Goethe Institute, Singapore and worked at Rohde & Schwarz at the same time as part of the Poly goes UAS Scholarship Program.",
   "Passing the B2/C1 Level German Language Test was a prerequisite for his Bachelor's Degree in Germany.",
   "Sreeroop completed his Secondary and Higher Secondary education in Science from Kendriya Vidyalaya, Kerala, India in 2018.",
   "<br>"
@@ -40,12 +43,16 @@ education = [
 skills = [
   "<br>",
   "Sreeroop's had the pleasure of diving into various projects across different Research and Development departments at both Rohde & Schwarz, Singapore and Rohde & Schwarz, Munich.",
-  "When it comes to building things Sreeroop is comfortable with Java, Python, C#, C++, C, Dart, TypeScript, SQL, and GO.",
-  "Sreeroop's time at R&S gave him opportunities to tackle ETL Pipelines and Big Data Projects using a combination of Python with Dagster for the ETL process, C# as Backend, and PostgresSQL as the database.",
-  "Docker and Kubernetes were used for containerization and orchestration of the services. He has also worked on a few projects using GO for building microservices.",
-  "Another interesting project that Sreeroop worked on was the firmware development of a RF device(STM32 based) used in R&S during EMC testing. Sreeroop used C, RTOS, and Python for the development of the firmware and the test automation.",
-  "For crafting web applications, you'll often find Sreeroop reaching for the trusty framework like React, Angular for frontend and Node.js or Spring Boot or .NET Core for Backend.",
-  "Flutter is Sreeroop's go-to for building mobile applications for both Android and iOS due to its State Management and UI capabilities, and it is is a great way to build cross-platform applications.",
+  "<br>",
+  "When it comes to building things, Sreeroop is proficient in Java, Python, C, C++, C#, TypeScript, and SQL. Additionally, he dedicates his free time to learning Rust and Go.",
+  "<br>",
+  "His time at R&S gave him opportunities to delve into Web Development, Scripting, ETL Pipelines and Big-Data Projects utilising a variety of frameworks and technlogies like Blazor, Angular, Node.js, .NET Core, PostgresSQL, Docker, Gitlab CI/CD, and more.",
+  "He has hands on experience in developing APIs based on REST, GraphQL and OData using Spring Boot, .NET Core and Python. He's also familiar with Docker files and containers for creating mircroservices.",
+  "Another interesting project that Sreeroop worked on was the firmware development of a RF device(STM32 based) used in R&S during EMC testing. Sreeroop used C and RTOS for the porting/development of the firmware.",
+  "He also possess expertise in developing test automation scripts using Python for testing the RF devices and the web applications developed by R&S.",
+  "<br>",
+  "For crafting web applications, you'll often find Sreeroop reaching for the trusty frameworks based on React, or Angular for frontend and Node.js or Spring Boot or .NET Core for Backend.",
+  "Flutter is his go-to for building mobile applications for both Android and iOS due to its State Management and UI capabilities, and it is is a great way to build cross-platform applications.",
   "<br>"
     ];
 
